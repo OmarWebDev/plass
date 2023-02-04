b99bdc58ecca52f56b77ebc196f4211cf14e7a0b
Omar Khaled <omar.khaled@merodev.com>
2023-02-04 02:30:43 +0200

    Add new methods

--- src/Facade/Str.php
+++ src/Facade/Str.php
@@ -2,15 +2,38 @@
 
 namespace Plass\Facade;
 
+use ArrayAccess;
 use Plass\Facade\Support\Stringable;
 
 class Str
 {
     use Stringable;
 
+    public function charAt(int $index)
+    {
+        return $this->string[$index];
+    }
+    
+    
     public function toUpperCase(): self
     {
         $this->string = strtoupper($this->string);
+        return $this;
+    }
+
+    public function toLowerCase(): self
+    {
+        $this->string = strtolower($this->string);
+        return $this;
+    }
+
+    public function repeat(int $count): string {
+        return str_repeat($this->string, $count);
+    }
+
+    public function trim(): self
+    {
+        $this->string = trim($this->string);
         return $this;
     }
 }
--- tests/Facade/StrTest.php
+++ tests/Facade/StrTest.php
@@ -2,7 +2,7 @@
 
 use Plass\Facade\Str;
 
-it('Should return a string when cast to print it', function () {
+it('should return a string when cast to print it', function () {
     $str = new Str('Hello World');
 
     expect((string)Str::of('Hello World'))->toBe('Hello World')
@@ -10,9 +10,31 @@ it('Should return a string when cast to print it', function () {
         ->and($str->toString())->toBe('Hello World');
 });
 
-it('Should return upper case string after upper casing it', function () {
+it('should return upper/lower case string after upper/lower casing it', function () {
     $str = Str::of('hello');
     $str->toUpperCase();
 
     expect($str->toString())->toBe('HELLO');
+
+    $str->toLowerCase();
+    expect($str->toString())->toBe('hello');
+});
+
+it('should return char at specific index', function () {
+   $str = Str::of('hello');
+
+   expect($str->charAt(0))->toBe('h');
+});
+
+it('should repeat string', function () {
+    $str = Str::of('_hello');
+
+    expect($str->repeat(1))->toBe('_hello')
+        ->and($str->repeat(2))->toBe('_hello_hello');
+});
+
+it('should trim string', function () {
+    $str = Str::of("   hello   ");
+    $str->trim();
+    expect($str->toString())->toBe('hello');
 });
